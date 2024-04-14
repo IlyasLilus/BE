45bfa04aeffda946d832cb46f384df3f9f1958db
Ilyas <jaballahilyas@gmail.com>
2024-04-14 17:03:55 +0200

    Fin de la page Accueil_Utilisateur

--- Accueil_Utilisateur.php
+++ Accueil_Utilisateur.php
@@ -20,4 +20,46 @@
             <div class="navbar-right">
                 <button class="Deconnexion">Déconnexion</button>
             </div>
+    </section>
+    </header>
+    <section class="Bienvenue">
+        <div class="Bienvenue-container">
+            <div>Bienvenue dans <span>votre<br>espace !</span></div>
+        </div>
+    </section>   
+    <section class="Projets">
+        <div class="Projets-container">
+            <div class="Projets-title">Mes Projets</div>
+            <div class="Projets-content">
+                <div class="Projets-content-container">
+                    <div class="Projets-content-container-title">Projet 1:</div>
+                    <div class="Projets-content-container-description">STRI_Achraf.miyaou</div>
+                    <div class="Projets-content-container-button-acceder"><button>Accéder</button></div>
+                    <div class="Projets-content-container-button-supprimer"><button>Supprimer</button></div>
+                </div>
+                <div class="Projets-content-container">
+                    <div class="Projets-content-container-title">Projet 2:</div>
+                    <div class="Projets-content-container-description">Ilyas_JABALLAH.miyaou</div>
+                    <div class="Projets-content-container-button-acceder"><button>Accéder</button></div>
+                    <div class="Projets-content-container-button-supprimer"><button>Supprimer</button></div>
+                </div>
+                <div class="Projets-content-container">
+                    <div class="Projets-content-container-title">Projet 3:</div>
+                    <div class="Projets-content-container-description">El_Mir2020.miyaou</div>
+                    <div class="Projets-content-container-button-acceder"><button>Accéder</button></div>
+                    <div class="Projets-content-container-button-supprimer"><button>Supprimer</button></div>
+                </div>
+            <div class="bouton-creation-container">
+                <button class="bouton-creation">Créer un nouveau projet</button>
+            </div>
+            </div>
+        </div>
+    </section>   
+    <footer>
+        <section class="footer-container">
+            <div class="footer-content">
+                <div>Copyright NetVision - Mention légal -Politique de confidentialité</div>
+            </div>
+        </section>
+    </footer>  
 <?>
