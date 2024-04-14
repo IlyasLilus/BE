f96ba00fc53f24e931f419b52a19093965dbe3bf
Ilyas <jaballahilyas@gmail.com>
2024-04-14 15:45:20 +0200

    Placement de l'image de background et début navbar

---
+++ Accueil_Utilisateur.php
@@ -0,0 +1,14 @@
+<php>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Accueil</title>
+    <link rel="stylesheet" type="text/css" href="style/style_accueil_utilisateur.css">
+</head>
+<body>
+    <img class="background" src="image/alexander-andrews-fsH1KjbdjE8-unsplash 1.png" alt="Background">
+    <header>
+    <section class="header-container">
+            <div class="navbar-header">
+                
+<?>
