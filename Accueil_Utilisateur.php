48fcd19e3bc9ba3e4b24fcffc07dbc3dcb0e1da9
Ilyas <jaballahilyas@gmail.com>
2024-04-14 16:09:30 +0200

    Fin navbar Accueil_Utilisateur

--- Accueil_Utilisateur.php
+++ Accueil_Utilisateur.php
@@ -9,6 +9,15 @@
     <img class="background" src="image/alexander-andrews-fsH1KjbdjE8-unsplash 1.png" alt="Background">
     <header>
     <section class="header-container">
-            <div class="navbar-header">
-                
+            <div class="navbar-left"></div>
+                <div><a href="Accueil.php">Accueil</a></div>
+                <div><a href="About.php">About</a></div>
+                <div><a href="Support.php">Support</a></div>
+            </div>
+            <div class="navbar-mid">
+                <img class="logo" src="image/Logo.png" alt="Logo">
+            </div>
+            <div class="navbar-right">
+                <button class="Deconnexion">Déconnexion</button>
+            </div>
 <?>
