d9da9ec7c6b0d3c7ee4f495f3c1fcd4d28c87a9b
Momo <mbenantar31@gmail.com>
2024-04-14 22:05:39 +0200

    Ajout de la page d'acceuil

---
+++ Accueil.php
@@ -0,0 +1,67 @@
+<php>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Accueil</title>
+    <link rel="stylesheet" type="text/css" href="style/style_accueil.css">
+</head>
+<body>
+    <img class="background" src="image/alexander-andrews-fsH1KjbdjE8-unsplash 1.png" alt="Background">
+    <header>
+    <section class="header-container">
+            <div class="navbar-left"></div>
+                <div><a href="Accueil.php">Accueil</a></div>
+                <div><a href="About.php">About</a></div>
+                <div><a href="Support.php">Support</a></div>
+            </div>
+            <div class="navbar-mid">
+                <img class="logo" src="image/Logo.png" alt="Logo">
+            </div>
+            <div class="navbar-right">
+                <button class="Compte">Mon compte</button>
+            </div>
+    </section>
+    </header>
+    <section class="Description">
+        <div>Simulations de routage avec NetVision<br>
+            NetVision est l’outil le plus <span class ="Description-highlight">performant</span> de sa  <span class ="Description-highlight">génération<span></div>
+    </section>
+    <section>
+        <img class="image1" src="image/nasa-Q1p7bh3SHj8-unsplash 1.png" alt="Image1">  
+        <div class="Card1">
+        </div>
+        <div class="Card2">
+
+        </div>
+        <div class="Card3">
+
+        </div>
+        <div class="Decouvrir-containter">
+            <div class="Decouvrir">Découvrir Maintenant !</div>
+        </div>
+    </section>
+    <section>
+        <div class="CTAsupport">
+            Un problème ?<br>
+            <span class="CTA-highlight">Envoyez un simple mail à nos <br>
+            équipes, vous recevrez une <br>
+            réponse sous 24h</span>
+        </div>
+        <div class="CTAprobleme">
+            Simulation <span class="CTA-highlight"> performante </span> et<br>
+            <span class="CTA-highlight"></span> modulable </span> Utilisable par les <br>
+            <span class="CTA-highlight"></span>professionnels</span> et les <span class="Description-highlight"></span>amateurs</span> <br>
+        </div>
+        <div class="CTAsimulation">
+            <span class="CTA-highlight">SIMULATION<br>
+            SUPPORT CLIENT</span><br>
+            7j/7 24h/24
+        </div>
+    <footer>
+        <section class="footer-container">
+            <div class="footer-content">
+                <div>Copyright NetVision©</div>
+            </div>
+        </section>
+    </footer>  
+</body>
