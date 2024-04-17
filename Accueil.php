bb69875b4e72e50b6be629c3353d3080a1a6f80d
Momo <mbenantar31@gmail.com>
2024-04-17 02:20:31 +0200

    Main content done, polishing in next commit

--- Accueil.php
+++ Accueil.php
@@ -7,6 +7,7 @@
 </head>
 <body>
     <img class="background" src="image/alexander-andrews-fsH1KjbdjE8-unsplash 1.png" alt="Background">
+    <section class="filter"></section>
     <header>
     <section class="header-container">
             <div class="navbar-left"></div>
@@ -18,7 +19,9 @@
                 <img class="logo" src="image/Logo.png" alt="Logo">
             </div>
             <div class="navbar-right">
-                <button class="Compte">Mon compte</button>
+                <a href="Accueil_Utilisateur.php">
+                    <button class="Compte">Mon compte</button>
+                </a>
             </div>
     </section>
     </header>
@@ -36,11 +39,10 @@
         <div class="Card3">
 
         </div>
-        <div class="Decouvrir-containter">
-            <div class="Decouvrir">Découvrir Maintenant !</div>
-        </div>
+        <button class="Decouvrir">Découvrir Maintenant !</button>
+        
     </section>
-    <section>
+    <section class="CTAcontainer">
         <div class="CTAsupport">
             Un problème ?<br>
             <span class="CTA-highlight">Envoyez un simple mail à nos <br>
@@ -57,6 +59,65 @@
             SUPPORT CLIENT</span><br>
             7j/7 24h/24
         </div>
+        <button class="Commencer">Commencer à simuler</button>
+    </section>
+    <section id="reviews">
+        <img class="image2" src="image/jj-ying-8bghKxNU1j0-unsplash 1.png" alt="Image2">
+        <div class="textSatisfait">
+            Des utilisateurs satisfaits
+        </div>
+        <section class="avis">
+        <button id="prev-arrow" class="leftarrow-container">
+            <img class="leftarrow" src="image/leftArrow.png" alt="leftarrow">
+        </button>
+        <button id="next-arrow" class="rightarrow-container">
+            <img class="rightarrow" src="image/rightArrow.png" alt="rightarrow">
+        </button>
+
+        </section>
+    </section>
+    <script>
+        document.addEventListener('DOMContentLoaded', (event) => {
+        const prevArrow = document.getElementById('prev-arrow');
+        const nextArrow = document.getElementById('next-arrow');  
+        const client1 = document.getElementById('Client1'); 
+        const client2 = document.getElementById('Client2'); 
+
+        client1.classList.add('review-box', 'active');
+        client2.classList.add('review-box');
+
+        prevArrow.addEventListener('click', () => {
+            client1.classList.toggle('active');
+            client2.classList.toggle('active');
+        });
+
+        nextArrow.addEventListener('click', () => {
+            client1.classList.toggle('active');
+            client2.classList.toggle('active');
+        });
+        });
+    </script>
+
+    <section class="review-box active" id="Client1">
+        <div class="Client1-Box"></div>
+        <div class="Client1-Name">Ashraf Belhiss</div>
+        <div class="Client1-Location">Toulouse, France</div>
+        <div class="Client1-Text">C’est l’application teh les fou wallah</div>
+        <img class="Client1-image" src="image/1698328915992.jpg" alt="Client1">
+    </section>
+    <section class="review-box" id="Client2">
+        <div class="Client1-Box"></div>
+        <div class="Client1-Name">André Aoun</div>
+        <div class="Client1-Location">Toulouse, France</div>
+        <div class="Client1-Text">Ce projet mérite un 20/20</div>
+        <img class="Client1-image" src="image/1698328915992.jpg" alt="Client1">
+    </section>
+    
+    <section class="Sponsors">
+        <div class="SponsorTitle">Nos collaborateur</div>
+        <img class="SponsorImage" src="image/sponsors.png" alt="Sponsor1">
+    </section>
+    
     <footer>
         <section class="footer-container">
             <div class="footer-content">
