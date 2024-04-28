43c1768f9618aabcd2c87b4688745c816b7ad62c
ashrafbelhiss <ashrafbelhiss@gmail.com>
2024-04-28 20:02:02 +0200

    a propos

---
+++ propos.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>À propos de NetVision</title>
+    <link rel="stylesheet" href="style/styles_propos.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@800&family=Teko:wght@300;600&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <header class="header">
+            <nav>
+                <ul>
+                    <li class="go"><a href="Accueil.php">Accueil</a></li>
+                    <li class="go"><a href="Contact.php">Support</a></li>
+                </ul>
+            </nav>
+        <div class="cont-header">
+            <div href=""><a href="#home"><img class="logo" src="image/netvision.png" alt=""></a></div>
+            <button class="connexion"><a href="Connexion.php" >Connexion</a></button>
+            <button class="commencer"><a href="Inscription.php" >Commencer</a></button>
+        </div>
+    </header>
+    <main>
+        <section class="about-netvision">
+            <h2>Qui sommes-nous ?</h2>
+            <p>Qui sommes-nous ?
+NetVision est le fruit du travail d'un groupe d'étudiants passionnés par les réseaux informatiques et les technologies de communication. Notre objectif est de simplifier la compréhension et la gestion du routage statique IP pour les étudiants et les débutants dans le domaine des réseaux.</p>
+        </section>
+        <section class="team">
+            <article class="member">
+                <img src="image/belhiss.jpeg" alt="Membre 1" class="left">
+                <div class="text">
+                    <h3>Achraf BELHISS</h3>
+                    <p>Etudiant en L3 IRT passionée par les réseaux et les systèmes</p>
+                </div>
+            </article>
+            <article class="member">
+                <div class="text">
+                    <h3>Youssef EL MIR</h3>
+                    <p>Etudiant en L3 IRT passionée par les réseaux et les systèmes</p>
+                </div>
+                <img src="image/elmir.jpeg" alt="Membre 2" class="right">
+            </article>
+            <article class="member">
+                <img src="image/jaballah.jpeg" alt="Membre 3" class="left">
+                <div class="text">
+                    <h3>Ilyas JABALLAH</h3>
+                    <p>Etudiant en L3 IRT passionée par la cybersécurité </p>
+                </div>
+            </article>
+            <article class="member">
+                <div class="text">
+                    <h3>Mohammed BENANTAR</h3>
+                    <p>Etudiant en L3 IRT passionée par le domaine de la cybersécurité</p>   
+                </div>
+                <img src="image/BENANTAR.jpeg" alt="Membre 2" class="right">
+            </article>
+        </section>
+        <section class="goals">
+            <h2>Notre projet</h2>
+            <p>Notre plateforme offre une simulation de routage statique IP accessible via une interface conviviale. NetVision permet à ses utilisateurs de définir des configurations de routage, de simuler des environnements réseau et de visualiser l'impact de leurs choix de routage.</p>
+        </section>
+    </main>
+</body>
+
+</html>
